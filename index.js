65039c63fd578d380d5d43821d1248c3d0bf9ba6
ohletamyleta <ohletamyleta@gmail.com>
2021-04-07 11:07:25 -0400

    add get request

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const endPoint = "http://localhost:3000/api/v1/weapons";
 
 document.addEventListener("DOMContentLoaded", () => {
-  alert("LOADED");
+  getWeapons();
 });
+
+function getWeapons() {
+  fetch(endPoint)
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+}
