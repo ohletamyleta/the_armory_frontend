2f69f99fd6fbb118c1facbdc436fab35293bc09e
ohletamyleta <ohletamyleta@gmail.com>
2021-04-10 13:45:01 -0400

    remove update to refactor

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const endPoint = "http://localhost:3000/api/v1/weapons";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getWeapons()
+
+  const createWeaponForm = document.querySelector('#create-weapon-form')
+
+  createWeaponForm.addEventListener('submit', (e) => createFormHandler(e) )
+
+ 
+});
+
+function getWeapons() {
+  fetch(endPoint)
+    .then((res) => res.json())
+    .then(weapons => {
+       weapons.data.forEach(weapon => {
+
+        const newWeapon = new Weapon(weapon.id, weapon.attributes)
+ 
+        document.querySelector("#weapon-container").innerHTML += newWeapon.renderWeaponCard();
+    })
+})
+}
+
+function createFormHandler(e) {
+ e.preventDefault();
+ const nameInput = document.querySelector("#input-name").value;
+ const descriptionInput = document.querySelector("#input-description").value;
+ const videoInput = document.querySelector("#input-video").value;
+ const imageInput = document.querySelector("#input-image").value;
+ const originInput = document.querySelector("#input-origin").value;
+ const categoryInput = document.querySelector("#categories").value;
+ const categoryId = parseInt(categoryInput);
+ postWeapon(nameInput, descriptionInput, videoInput, imageInput, originInput, categoryInput);
+
+}
+
+function postWeapon(name, description, video_url, image_url, origin, category_id) {
+  
+  let bodyData = {name, description, video_url, image_url, origin, category_id};
+   fetch(endPoint, {
+     method: "POST",
+     headers: { "Content-Type": "application/json" },
+     body: JSON.stringify(bodyData)
+   })
+     .then(response => response.json())
+     .then((weapon) => {
+
+        const newWeapon = new Weapon(weapon.data.id, weapon.data.attributes);
+
+        document.querySelector("#weapon-container").innerHTML += newWeapon.renderWeaponCard();
+    
+    });
+  }
+    
+
+
+
+
+
---
+++ src/weapon.js
@@ -0,0 +1,42 @@
+class Weapon {
+  constructor(id, weaponAttributes) {
+    this.id = id;
+    this.name = weaponAttributes.name;
+    this.description = weaponAttributes.description;
+    this.image_url = weaponAttributes.image_url;
+    this.video_url = weaponAttributes.video_url;
+    this.origin = weaponAttributes.origin;
+    this.category = weaponAttributes.category;
+    Weapon.all.push(this);
+  }
+
+  renderWeaponCard() {
+    return `
+    <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src=${this.image_url} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h3 class="card-text">${this.name}</h3>
+              <h4 class="card-text"><em>Category: ${this.category.name}</em></h4>
+              <h5 class="card-text">Origin: ${this.origin}</h5>
+              <p class="card-text">${this.description}</p>
+              <a href="${this.video_url}">See it in action!</a>
+               <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                 <button type="button" class="btn btn-primary" id="update-weapon">Edit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  }
+
+
+  static findById = (id) => {
+    return this.all.find((weapon) => parseInt(weapon.id) === id);
+  };
+
+}
+Weapon.all = [];
