1789869487ebb29d90a2e7991df0c968d5822fa5
ohletamyleta <ohletamyleta@gmail.com>
2021-04-10 13:38:32 -0400

    remove update to refactor

--- src/index.js
+++ src/index.js
@@ -7,15 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   createWeaponForm.addEventListener('submit', (e) => createFormHandler(e) )
 
-  const weaponContainer = document.querySelector('#weapon-container')
-  
-  weaponContainer.addEventListener('click', e => {
-     const id = parseInt(e.target.dataset.id);
-     const weapon = Weapon.findById(id);
-     document.querySelector("#update-weapon").innerHTML = weapon.renderUpdateForm();
-   });
-    // document.querySelector('#update-weapon').addEventListener('submit', e => updateFormHandler(e))
-
+ 
 });
 
 function getWeapons() {
@@ -25,7 +17,7 @@ function getWeapons() {
        weapons.data.forEach(weapon => {
 
         const newWeapon = new Weapon(weapon.id, weapon.attributes)
-
+ 
         document.querySelector("#weapon-container").innerHTML += newWeapon.renderWeaponCard();
     })
 })
@@ -47,9 +39,7 @@ function createFormHandler(e) {
 function postWeapon(name, description, video_url, image_url, origin, category_id) {
   
   let bodyData = {name, description, video_url, image_url, origin, category_id};
-
    fetch(endPoint, {
-    
      method: "POST",
      headers: { "Content-Type": "application/json" },
      body: JSON.stringify(bodyData)
@@ -57,7 +47,6 @@ function postWeapon(name, description, video_url, image_url, origin, category_id
      .then(response => response.json())
      .then((weapon) => {
 
-     
         const newWeapon = new Weapon(weapon.data.id, weapon.data.attributes);
 
         document.querySelector("#weapon-container").innerHTML += newWeapon.renderWeaponCard();
@@ -65,36 +54,6 @@ function postWeapon(name, description, video_url, image_url, origin, category_id
     });
   }
     
-function updateFormHandler(e) {
-  e.preventDefault();
-
-  const id = parseInt(e.target.dataset.id);
-  const weapon = Weapon.findById(id);
-  const name = document.querySelector("#update-name").value;
-  const description = document.querySelector("#update-description").value;
-  const video_url = document.querySelector("#update-video-url").value;
-  const image_url = document.querySelector("#update-image-url").value;
-  const origin = document.querySelector("#update-origin").value;
-  const category = document.querySelector("#categories").value;
-
-
-  patchWeapon(id, name, description, video_url, image_url, origin, category)
-
-}
- 
-function patchWeapon(id, name, description, video_url, image_url, origin, category) {
-  const bodyJSON = { id, name, description, video_url, image_url, origin, category }
-  fetch(`http://localhost:3000/api/v1/weapons/${id}`, {
-    method: 'PATCH',
-    headers: { 
-      "Content-Type": "application/json", 
-      "Accept": "application/json" },
-    body: JSON.stringify(bodyJSON),
-  })
-    .then(res => res.json())
-    .then(console.log("updatedWeapon"));
-
-    };
 
 
 
--- src/weapon.js
+++ src/weapon.js
@@ -23,7 +23,7 @@ class Weapon {
               <a href="${this.video_url}">See it in action!</a>
                <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                <button type="button" class="btn btn-primary" id="update-weapon">Edit</button>
                 </div>
               </div>
             </div>
@@ -31,53 +31,13 @@ class Weapon {
         </div>
       </div>
     </div>`;
-
   }
- 
-    static findById = id => {
-      return this.all.find((weapon) => parseInt(weapon.id) === id);
-    }
 
-    renderUpdateForm = () => {
-      // create a modal fxn for this
-      return `
-      <form data-id=${this.id} >
-        <h3>Modify A Weapon!</h3>
 
-        <label>Name</label>
-        <input id='update-name' type="text" name="name" value="${this.name}" class="input-text">
-        <br><br>
+  static findById = (id) => {
+    return this.all.find((weapon) => parseInt(weapon.id) === id);
+  };
 
-        <label>Origin</label>
-        <textarea id='update-origin' type="text" name="name" rows="4" cols="40" value="">${this.origin}</textarea>
-        <br><br>
-
-        <label>Description</label>
-        <textarea id='update-description' name="description" rows="8" cols="80" value="">${this.description}</textarea>
-        <br><br>
-
-        <label>Video Link</label>
-        <input id='update-video-url' type="text" name="image" value="${this.video_url}" class="input-text">
-        <br><br>
-      
-        <label>Image Link</label>
-        <input id='update-image-url' type="text" name="image" value="${this.image_url}" class="input-text">
-        <br><br>
-      
-        <label>Category</label>
-        <select id="categories" name="categories" value="${this.category.name}">
-        <option value="1">Blunt</option>
-        <option value="2">Sharp</option>
-        <option value="3">Projectile</option>
-        <option value="4">Legendary</option>
-        <option value="5">Nonconformist</option>
-        </select>
-        <br><br>
-
-      <input id='edit-button' type="submit" name="submit" value="Modify Weapon" class="submit">
-    </form>
-      `;
-    }
 }
 
 Weapon.all = [];
